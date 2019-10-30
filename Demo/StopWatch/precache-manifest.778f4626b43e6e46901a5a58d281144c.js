aeb2a7f752862d43ff104413983e5f1189ea4465
Tanya Ly <tanya.lytovchenko@gmail.com>
2019-10-30 03:19:47 +0100

    Test github pages

---
+++ Demo/StopWatch/precache-manifest.778f4626b43e6e46901a5a58d281144c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e90dd0059aac76667e5bce0c1215571",
+    "url": "/Demo/StopWatch/index.html"
+  },
+  {
+    "revision": "04dfd8d0005ce1684b70",
+    "url": "/Demo/StopWatch/static/css/main.e77ecb0b.chunk.css"
+  },
+  {
+    "revision": "4689be5a588fe55b2294",
+    "url": "/Demo/StopWatch/static/js/2.10f41c9b.chunk.js"
+  },
+  {
+    "revision": "04dfd8d0005ce1684b70",
+    "url": "/Demo/StopWatch/static/js/main.55663600.chunk.js"
+  },
+  {
+    "revision": "84685926c2c1692afb2d",
+    "url": "/Demo/StopWatch/static/js/runtime-main.046f68ff.js"
+  }
+]);
