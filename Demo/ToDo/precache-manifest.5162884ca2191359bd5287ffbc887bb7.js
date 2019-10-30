5a0bca08371fdc58f76277122fbc720f07cdf93b
Tanya Ly <tanya.lytovchenko@gmail.com>
2019-10-30 23:57:07 +0100

    ToDo app Demo

---
+++ Demo/ToDo/precache-manifest.5162884ca2191359bd5287ffbc887bb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1a5119b4dfba6d051b7e6b35914e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac1283a9301b110896c",
+    "url": "/static/css/main.e647682a.chunk.css"
+  },
+  {
+    "revision": "08e8d9c5afb66c0cc04c",
+    "url": "/static/js/2.89ade69e.chunk.js"
+  },
+  {
+    "revision": "9ac1283a9301b110896c",
+    "url": "/static/js/main.447acd91.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
